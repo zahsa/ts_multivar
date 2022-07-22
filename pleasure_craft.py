cd2973063725e047828930c395dc7541af4d60d9
marthadais <dais.martha@gmail.com>
2022-07-22 14:21:34 -0300

    fizing path bug

--- pleasure_craft.py
+++ pleasure_craft.py
@@ -35,7 +35,7 @@ if not os.path.exists(features_path_arima):
     features_arima = features_arima.coeffs
 else:
     print('arima coeffs exist')
-    features_arima = pd.read_csv(features_path_ou, index_col=0)
+    features_arima = pd.read_csv(features_path_arima, index_col=0)
 features_path_var = f'./results/DCAIS_example/VAR/features_coeffs.csv'
 if not os.path.exists(features_path_var):
     print('Univariate Multivariate...')
@@ -43,39 +43,81 @@ if not os.path.exists(features_path_var):
     features = features.coeffs
 else:
     print('var coeffs exist')
-    features = pd.read_csv(features_path_ou, index_col=0)
+    features = pd.read_csv(features_path_var, index_col=0)
 
 ### Runing clustering OU
 print('Clustering OU...')
+
 # result_ou_1 = Clustering(ais_data_path=dataset.preprocessed_path, dm=features_ou, cluster_algorithm='dbscan', folder=f'./results/DCAIS_example/OU/', norm_dist=False)
-result_ou_2 = Clustering(ais_data_path=dataset.preprocessed_path, dm=features_ou, cluster_algorithm='hierarchical', linkage='average', k=5, folder=f'./results/DCAIS_example/OU/', norm_dist=False)
-result_ou_3 = Clustering(ais_data_path=dataset.preprocessed_path, dm=features_ou, cluster_algorithm='spectral', k=5, folder=f'./results/DCAIS_example/OU/', norm_dist=False)
+
+cl_path_ou_hc = './results/DCAIS_example/OU/hierarchical-average/hierarchical_5_average.csv'
+if not os.path.exists(cl_path_ou_hc):
+    result_ou_2 = Clustering(ais_data_path=dataset.preprocessed_path, dm=features_ou, cluster_algorithm='hierarchical', linkage='average', k=5, folder=f'./results/DCAIS_example/OU/', norm_dist=False)
+    result_ou_2 = result_ou_2.labels
+else:
+    print('HC 5 exist')
+    result_ou_2 = pd.read_csv(cl_path_ou_hc)['Clusters']
+
+cl_path_ou_spectral = './results/DCAIS_example/OU/spectral/spectral_5.csv'
+if not os.path.exists(cl_path_ou_spectral):
+    result_ou_3 = Clustering(ais_data_path=dataset.preprocessed_path, dm=features_ou, cluster_algorithm='spectral', k=5, folder=f'./results/DCAIS_example/OU/', norm_dist=False)
+    result_ou_3 = result_ou_3.labels
+else:
+    print('SC 5 exist')
+    result_ou_2 = pd.read_csv(cl_path_ou_hc)['Clusters']
 
 ### Runing clustering ARIMA
 print('Clustering ARIMA...')
 # result_arima_1 = Clustering(ais_data_path=dataset.preprocessed_path, dm=features_arima, cluster_algorithm='dbscan', folder=f'./results/DCAIS_example/ARIMA/', norm_dist=False)
-result_arima_2 = Clustering(ais_data_path=dataset.preprocessed_path, dm=features_arima, cluster_algorithm='hierarchical', linkage='average', k=5, folder=f'./results/DCAIS_example/ARIMA/', norm_dist=False)
-result_arima_3 = Clustering(ais_data_path=dataset.preprocessed_path, dm=features_arima, cluster_algorithm='spectral', k=5, folder=f'./results/DCAIS_example/ARIMA/', norm_dist=False)
+
+cl_path_arima_hc = './results/DCAIS_example/ARIMA/hierarchical-average/hierarchical_5_average.csv'
+if not os.path.exists(cl_path_arima_hc):
+    result_arima_2 = Clustering(ais_data_path=dataset.preprocessed_path, dm=features_arima, cluster_algorithm='hierarchical', linkage='average', k=5, folder=f'./results/DCAIS_example/ARIMA/', norm_dist=False)
+    result_arima_2 = result_arima_2.labels
+else:
+    print('SC 5 exist')
+    result_arima_2 = pd.read_csv(cl_path_ou_hc)['Clusters']
+
+cl_path_arima_spectral = './results/DCAIS_example/ARIMA/spectral/spectral_5.csv'
+if not os.path.exists(cl_path_arima_spectral):
+    result_arima_3 = Clustering(ais_data_path=dataset.preprocessed_path, dm=features_arima, cluster_algorithm='spectral', k=5, folder=f'./results/DCAIS_example/ARIMA/', norm_dist=False)
+    result_arima_3 = result_arima_3.labels
+else:
+    print('SC 5 exist')
+    result_arima_3 = pd.read_csv(cl_path_ou_hc)['Clusters']
 
 ### Runing clustering
 print('Clustering VAR...')
 # result_1 = Clustering(ais_data_path=dataset.preprocessed_path, dm=features, cluster_algorithm='dbscan', folder=f'./results/DCAIS_example/', norm_dist=False)
-result_2 = Clustering(ais_data_path=dataset.preprocessed_path, dm=features, cluster_algorithm='hierarchical', linkage='average', k=5, folder=f'./results/DCAIS_example/', norm_dist=False)
-result_3 = Clustering(ais_data_path=dataset.preprocessed_path, dm=features, cluster_algorithm='spectral', k=5, folder=f'./results/DCAIS_example/', norm_dist=False)
 
+cl_path_var_hc = './results/DCAIS_example/hierarchical-average/hierarchical_5_average.csv'
+if not os.path.exists(cl_path_var_hc):
+    result_2 = Clustering(ais_data_path=dataset.preprocessed_path, dm=features, cluster_algorithm='hierarchical', linkage='average', k=5, folder=f'./results/DCAIS_example/', norm_dist=False)
+    result_2 = result_2.labels
+else:
+    print('SC 5 exist')
+    result_2 = pd.read_csv(cl_path_ou_hc)['Clusters']
+
+cl_path_var_spectral = './results/DCAIS_example/spectral/spectral_5.csv'
+if not os.path.exists(cl_path_var_spectral):
+    result_3 = Clustering(ais_data_path=dataset.preprocessed_path, dm=features, cluster_algorithm='spectral', k=5, folder=f'./results/DCAIS_example/', norm_dist=False)
+    result_3 = result_3.labels
+else:
+    print('SC 5 exist')
+    result_3 = pd.read_csv(cl_path_var_spectral)['Clusters']
 
 #NMI
 print('OU vs ARIMA')
 # print(normalized_mutual_info_score(result_ou_1.labels, result_arima_1.labels))
-print(normalized_mutual_info_score(result_ou_2.labels, result_arima_2.labels))
-print(normalized_mutual_info_score(result_ou_3.labels, result_arima_3.labels))
+print(normalized_mutual_info_score(result_ou_2, result_arima_2))
+print(normalized_mutual_info_score(result_ou_3, result_arima_3))
 
 print('ARIMA vs VAR')
 # print(normalized_mutual_info_score(result_arima_1.labels, result_1.labels))
-print(normalized_mutual_info_score(result_arima_2.labels, result_2.labels))
-print(normalized_mutual_info_score(result_arima_3.labels, result_3.labels))
+print(normalized_mutual_info_score(result_arima_2, result_2))
+print(normalized_mutual_info_score(result_arima_3, result_3))
 
 print('OU vs VAR')
 # print(normalized_mutual_info_score(result_ou_1.labels, result_1.labels))
-print(normalized_mutual_info_score(result_ou_2.labels, result_2.labels))
-print(normalized_mutual_info_score(result_ou_3.labels, result_3.labels))
+print(normalized_mutual_info_score(result_ou_2, result_2))
+print(normalized_mutual_info_score(result_ou_3, result_3))
